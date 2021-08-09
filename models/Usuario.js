4a04e6d635453d72caa613d393b148f4255a9c92
Luis Alberto <luisbc_1998@hotmail.com>
2021-08-09 14:49:31 -0500

    model user created

---
+++ models/Usuario.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const UsuarioSchema = Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	lastName: {
+		type: String,
+		required: true,
+	},
+	email: { type: String, required: true, uniqued: true, trim: true },
+	password: {
+		type: String,
+		required: true,
+	},
+	created: {
+		type: Date,
+		default: Date.now(),
+	},
+});
+
+module.exports = model('Usuaio', UsuarioSchema);
