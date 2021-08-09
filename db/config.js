dd43c688eef6b39fb446fce72a66e6082c2a6019
Luis Alberto <luisbc_1998@hotmail.com>
2021-08-09 13:38:07 -0500

    db conected

---
+++ db/config.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const dbConnection = async () => {
+	try {
+		mongoose.connect(process.env.DB_CNN, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		console.log('DB Online');
+	} catch (error) {
+		console.log(error);
+		throw new Error('Error al momento de inicialiar la base de datos');
+	}
+};
+
+module.exports = { dbConnection };
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
-
 const app = express();
+require('dotenv').config();
+const { dbConnection } = require('./db/config');
+
+dbConnection();
 
 app.listen(4000, () => {
 	console.log(`Servidor corriendo en puerto: ${4000}`);
